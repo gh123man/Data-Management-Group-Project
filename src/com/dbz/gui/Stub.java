cd19b6bcdedd2b51bf4bd829d553e12c3307a60e
Pat Gleason <pat.j.gleason@gmail.com>
2016-03-18 17:24:07 -0400

    Test commit.

--- src/com/dbz/gui/Stub.java
+++ src/com/dbz/gui/Stub.java
@@ -2,6 +2,7 @@ package com.dbz.gui;
 
 /**
  * Created by brian on 3/16/16.
+ *  Viewed by Pat Gleason 3/18/2016
  */
 public class Stub { //Please delete me when you add real code to this package
 }
